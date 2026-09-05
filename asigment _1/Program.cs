59d10e86fad70e21825b4e031c84f8c3084c1bc4
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-05 21:05:55 +0200

    q5

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -6,24 +6,9 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region Write code that divides 10 by 0 inside a try block, catches the exception, prints "Cannot divide by zero", and then prints "Done" in a finally block.
-            try
-            {
-                // divice 10 by 0 
-                int num = 10;
-                int zero = 0;
-                int result = num / zero;
-            }
-
-            catch (Exception)
-            {
-                WriteLine("Cannot divide by zero");
-            }
-
-            finally
-            {
-                WriteLine("done");
-            }
+            #region Declare an int pages = 300; then store it in a double variable without using a cast.
+            int pages = 300;
+            double doublePages = pages; 
             #endregion
         }
 
