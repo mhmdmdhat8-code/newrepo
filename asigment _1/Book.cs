daaf9f208a95118648aec98b1a446d6f871f2df2
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-05 20:47:00 +0200

    Add project files.

---
+++ asigment _1/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asigment__1
+{
+    public class Book
+    {
+        public string title;
+        public int page;
+        public override string ToString()
+        {
+            return $"{title},{page}";
+        }
+    }
+}
---
+++ asigment _1/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+namespace asigment__1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Look at the line below. Is it a compile-time error, a runtime error, or a logical error? Fix it.
+            // هعمل نسخة من الكتاب واملي بيانتها 
+            Book mybook = new Book();
+            mybook.title = "C# Programming";
+            mybook.page = 464;
+            // هخزن النسخة دي جوه متغير من نوع object 
+            Object objbook = mybook;
+            // هنطبع المتغير 
+            WriteLine(objbook);
+            // هستخدم الاربع دوال الاساسية المورورثين من الاب واعملهم overrite
+            WriteLine(objbook.ToString());
+            WriteLine(objbook.Equals(objbook));
+            WriteLine(objbook.GetHashCode());
+            WriteLine(objbook.GetType());
+
+            #endregion
+
+        }
+
+
+    }
+}
