3da881a0ee68ada81b1edadbbe0d5e2b8cf0a0e1
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-08 16:44:32 +0200

    q10

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -12,12 +12,21 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region Given int pages = 464; and bool isAvailable = true;, print "You can borrow this book" only if pages is greater than 300 and isAvailable is true.Use the && operator.
-            int pages = 464;
-            bool isAvailable = true;
-            if (pages > 300 && isAvailable)
+            #region Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+            string title = "Refactoring";
+            switch (title)
             {
-                WriteLine("You can borrow this book");
+                case "Clean Code":
+                    WriteLine("Great choice!");
+                    break;
+
+                case "Refactoring":
+                    WriteLine("Nice pick!");
+                    break;
+
+                default:
+                    WriteLine("Never heard of it");
+                    break;
             }
             #endregion
         }
