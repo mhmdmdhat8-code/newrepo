a14a009a4b8f5578bc606d89c8b775a795f71216
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-08 16:45:13 +0200

    q11

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -12,22 +12,10 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
-            string title = "Refactoring";
-            switch (title)
-            {
-                case "Clean Code":
-                    WriteLine("Great choice!");
-                    break;
-
-                case "Refactoring":
-                    WriteLine("Nice pick!");
-                    break;
-
-                default:
-                    WriteLine("Never heard of it");
-                    break;
-            }
+            #region Given int pages = 464;, use the ternary operator to store "Long Book" or "Short Book" into a variable sizeLabel(same rule as question 8: long if pages > 300).
+            int pages = 464;
+            string sizeLabel = (pages > 300) ? "Long Book" : "Short Book";
+            WriteLine(sizeLabel);
             #endregion
         }
     }
