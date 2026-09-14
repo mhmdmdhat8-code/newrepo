196789f118bfdbf95cd4e465e538ce87a26fa1b8
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-14 19:18:26 +0200

    q2

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Channels;
 using static System.Console;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace asigment__1
 {
 
@@ -12,7 +13,7 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region 
+            #region Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
             int[,] shelfCopies =
             {
                 {3,5},{1,4}
