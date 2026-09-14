c4f5e9a8c5e88f5c082f263df269c4212c4e71e9
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-14 19:21:51 +0200

    q3

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -13,14 +13,15 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
-            int[,] shelfCopies =
-            {
-                {3,5},{1,4}
-            };
-            WriteLine(shelfCopies[1, 0]);
+            #region Write a method called PrintWelcomeMessage that takes no parameters and prints "Welcome to the Library!".Call it from Main.
+            PrintWelcomeMessage();
             #endregion
 
         }
+        static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Library!");
+        }
     }
+
     }  
