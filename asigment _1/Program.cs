a516b4ab9181589c501a2c8d77b946833f76639b
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-18 19:40:58 +0200

    q4

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -16,21 +16,28 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region Add a public string Title; field to Book. Set it and print it from Main.
-            Book mybook = new Book();
-            mybook.Title = "Clean Code";
-            WriteLine(mybook.Title);
+            #region Declare an enum Genre { Fiction, NonFiction, Science }. Add a Genre property to Book, assign it Genre.Science, and print it.
+            Book myBook = new Book();
+
+            myBook.Title = "Quantum Physics";
+            myBook.BookGenre = Genre.Science;
+            WriteLine($"Book: {myBook.Title}, Genre: {myBook.BookGenre}");
             #endregion
 
         }
 
         class Book
         {
-            public string Title;
-            private string password = "secret";
-            internal int copiesInStock = 5;
+            public string Title { get; set; }
+            public Genre BookGenre { get; set; }
+
         }
 
-
+        enum Genre
+        {
+            Fiction,
+            NonFiction,
+            Science
+        }
     }
 }
