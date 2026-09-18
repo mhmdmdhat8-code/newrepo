ab9497989ecb8675489c1a76d0acd88c4a7183bc
mhmdmdhat8-code <mhmdmdhat8@gmail.com>
2026-09-18 19:27:45 +0200

    q3

--- asigment _1/Program.cs
+++ asigment _1/Program.cs
@@ -16,19 +16,17 @@ namespace asigment__1
     {
         static void Main(string[] args)
         {
-            #region Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why?
-                        Book mybook = new Book();
-            WriteLine($"Copies in stock: {mybook.copiesInStock}");
-
-            // does it compile? yes 
-            // why: لأن كلمة internal بتسمح لك بالوصول للمتغير داخل المشروع الواحد 
+            #region Add a public string Title; field to Book. Set it and print it from Main.
+            Book mybook = new Book();
+            mybook.Title = "Clean Code";
+            WriteLine(mybook.Title);
             #endregion
 
         }
 
         class Book
         {
-            public string Title = "Clean Code";
+            public string Title;
             private string password = "secret";
             internal int copiesInStock = 5;
         }
